729a535eeb6aecb49d62a46d33a6dd512a874cf0
邹明哲 <zoumingzhe@qq.com>
2020-04-16 20:43:20 +0800

    Update 445.add-two-numbers-ii.c

--- problems/445.add-two-numbers-ii.c
+++ problems/445.add-two-numbers-ii.c
@@ -6,74 +6,91 @@
  * };
  */
 
+typedef struct StackLink{
+    void *item;
+    struct StackLink *prev;
+}StackLink;
+
+StackLink *push(StackLink *s, void *item)
+{
+    StackLink *ret;
+    ret = (StackLink*)malloc(sizeof(StackLink));
+    ret->item = item;
+    ret->prev = s;
+    return ret;
+}
+
+StackLink *pop(StackLink *s)
+{
+	StackLink *ret = NULL;
+	if (s)
+	{
+		ret = s->prev;
+		free(s);
+	}
+	return ret;
+}
+
+void *get_top(StackLink *s)
+{
+	return s ? s->item : NULL;
+}
 
 struct ListNode* addTwoNumbers(struct ListNode* l1, struct ListNode* l2){
-    struct ListNode* item1;
-    struct ListNode* item2;
-    struct ListNode* item3;
-    struct ListNode* newl1;
-    struct ListNode* newl2;
-    struct ListNode* tmp;
+    StackLink *stack1 = NULL;
+    StackLink *stack2 = NULL;
+    struct ListNode* item1 = NULL;
+    struct ListNode* item2 = NULL;
+    struct ListNode* tmp = NULL;
     struct ListNode* ret = NULL;
     int       add = 0;
 
-    // 反转列表1
-    item1 = l1;
-    item2 = l1->next;
-    l1->next = NULL;
-    while (item2 != NULL)
+    for (item1 = l1; item1 != NULL ; item1 = item1->next)
     {
-        item3 = item2->next;
-        item2->next = item1;
-        item1 = item2;
-        item2 = item3;
+        stack1 = push(stack1, item1);
     }
-    newl1 = item1;
 
-    // 反转列表2
-    item1 = l2;
-    item2 = l2->next;
-    l2->next = NULL;
-    while (item2 != NULL)
+    for (item2 = l2; item2 != NULL ; item2 = item2->next)
     {
-        item3 = item2->next;
-        item2->next = item1;
-        item1 = item2;
-        item2 = item3;
+        stack2 = push(stack2, item2);
     }
-    newl2 = item1;
 
     // 相加
-    while (newl1 != NULL || newl2 != NULL)
+    while (1)
     {
+        item1 = (struct ListNode*)get_top(stack1);
+        item2 = (struct ListNode*)get_top(stack2);
+        stack1 = pop(stack1);
+        stack2 = pop(stack2);
+
+        if (item1 != NULL && item2 != NULL)
+        {
+            add += (item1->val + item2->val);
+        }
+        else if (item1 != NULL)
+        {
+            add += (item1->val);
+        }
+        else if (item2 != NULL)
+        {
+            add += (item2->val);
+        }
+        else
+        {
+            if (add > 0)
+            {
+                tmp = (struct ListNode*)malloc(sizeof(struct ListNode));
+                tmp->val = add;
+                tmp->next = ret;
+                ret = tmp;
+            }
+            return ret;
+        }
+        
         tmp = (struct ListNode*)malloc(sizeof(struct ListNode));
-        if (newl1 != NULL && newl2 != NULL)
-        {
-            add += (newl1->val + newl2->val);
-            newl1 = newl1->next;
-            newl2 = newl2->next;
-        }
-        else if (newl1 != NULL)
-        {
-            add += (newl1->val);
-            newl1 = newl1->next;
-        }
-        else if (newl2 != NULL)
-        {
-            add += (newl2->val);
-            newl2 = newl2->next;
-        }
         tmp->val = add % 10;
         tmp->next = ret;
         ret = tmp;
         add /= 10;
     }
-    if (add > 0)
-    {
-        tmp = (struct ListNode*)malloc(sizeof(struct ListNode));
-        tmp->val = add;
-        tmp->next = ret;
-        ret = tmp;
-    }
-    return ret;
 }
