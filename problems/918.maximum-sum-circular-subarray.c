01df6b6564ee1f489985af9cbfeb488f91d92937
邹明哲 <zoumingzhe@qq.com>
2020-04-29 17:03:29 +0800

    Update 918.maximum-sum-circular-subarray.c

--- problems/918.maximum-sum-circular-subarray.c
+++ problems/918.maximum-sum-circular-subarray.c
@@ -1,9 +1,15 @@
 #define max(a,b) (a>b?a:b)
+#define is_merge(a,b) ((a>=0&&b>=0)?1:0)
 
-int max_sub(int *arr, int num, int off)
+int max_val(int *arr, int num, int off)
 {
     int sum = arr[off];
     int max = arr[off];
+    if (max < 0)
+    {
+        return max;
+    }
+    
     for (int i = off + 1; i < off + num; i++)
     {
         sum += arr[i % num];
@@ -12,13 +18,33 @@ int max_sub(int *arr, int num, int off)
     return max;
 }
 
-int maxSubarraySumCircular(int* A, int ASize){
-    int val = A[0];
-    int max = A[0];
-    for (int i = 0; i < ASize; i++)
+int max_sub(int *arr, int num)
+{
+    int val = arr[0];
+    int max = arr[0];
+    for (int i = 0; i < num; i++)
     {
-        val = max_sub(A, ASize, i);
+        val = max_val(arr, num, i);
         max = max(max, val);
     }
     return max;
+}
+
+int maxSubarraySumCircular(int* A, int ASize){
+    int *B = (int*)malloc(sizeof(int)*ASize);
+    int BSize;
+    BSize = 1;
+    B[0] = A[0];
+    for (int off = 1; off < ASize; off++)
+    {
+        if (is_merge(A[off], B[BSize-1]))
+        {
+            B[BSize-1] += A[off];
+        }
+        else
+        {
+            B[BSize++] = A[off];
+        }
+    }
+    return max_sub(B, BSize);
 }
