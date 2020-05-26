52ad3da4480a65b224338f2f53f94367fe3c9a86
邹明哲 <zoumingzhe@qq.com>
2020-05-26 10:15:08 +0800

    Update 287.find-the-duplicate-number.c

--- problems/287.find-the-duplicate-number.c
+++ problems/287.find-the-duplicate-number.c
@@ -1,3 +1,13 @@
 int findDuplicate(int* nums, int numsSize){
-
+    for (int i = 0; i < numsSize; i++)
+    {
+        for (int j = i+1; j < numsSize; j++)
+        {
+            if (nums[i] == nums[j])
+            {
+                return nums[i];
+            }
+        }
+    }
+    return 0;
 }
