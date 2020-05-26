bd42fc0c8cc0122d0215e897583bf4f7960393f6
邹明哲 <zoumingzhe@qq.com>
2020-05-26 10:20:46 +0800

    Update 287.find-the-duplicate-number.c

--- problems/287.find-the-duplicate-number.c
+++ problems/287.find-the-duplicate-number.c
@@ -1,12 +1,46 @@
+void quictsort(int* nums, int head, int tail) {
+	int i = head;
+	int j = tail;
+	int tmp = nums[i];
+	while (i < j)
+	{
+		for (; j > i; j--)
+		{
+			if (nums[j] < tmp)
+			{
+				nums[i] = nums[j];
+				break;
+			}
+		}
+		for (; i < j; i++)
+		{
+			if (nums[i] > tmp)
+			{
+				nums[j] = nums[i];
+				break;
+			}
+		}
+	}
+	nums[i] = tmp;
+	if (i - head > 1)
+	{
+		quictsort(nums, head, i - 1);
+	}
+	if (tail - j > 1)
+	{
+		quictsort(nums, j + 1, tail);
+	}
+}
+
 int findDuplicate(int* nums, int numsSize){
-    for (int i = 0; i < numsSize; i++)
+    int *tmp = (int*)malloc(sizeof(int)*numsSize);
+    memcpy(tmp, nums, sizeof(int)*numsSize);
+    quictsort(tmp, 0, numsSize-1);
+    for (int i = 0; i < numsSize-1; i++)
     {
-        for (int j = i+1; j < numsSize; j++)
+        if (tmp[i] == tmp[i+1])
         {
-            if (nums[i] == nums[j])
-            {
-                return nums[i];
-            }
+            return tmp[i];
         }
     }
     return 0;
