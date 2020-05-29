9aba9ade07752b6753872a4f81579c5a21c74ce4
邹明哲 <zoumingzhe@qq.com>
2020-05-29 09:24:01 +0800

    error1
    case：[2,1,1,2]
    output：3
    result：4

--- problems/198.house-robber.c
+++ problems/198.house-robber.c
@@ -1,3 +1,20 @@
-int rob(int* nums, int numsSize){
+#define max(a,b) (a) > (b) ? (a) : (b)
 
+int rob(int* nums, int numsSize){
+    int a = 0;
+    int b = 0;
+
+    for (int i = 0; i < numsSize; i++)
+    {
+        if (i % 2 == 0)
+        {
+            a += nums[i];
+        }
+        else
+        {
+            b += nums[i];
+        }
+    }
+    
+    return max(a,b);
 }
