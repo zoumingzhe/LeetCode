be4cccbe05bdd630db614c5fbd1eaddcd541178c
邹明哲 <zoumingzhe@qq.com>
2020-06-04 16:32:06 +0800

    Update 238.product-of-array-except-self.c
    division

--- problems/238.product-of-array-except-self.c
+++ problems/238.product-of-array-except-self.c
@@ -2,5 +2,19 @@
  * Note: The returned array must be malloced, assume caller calls free().
  */
 int* productExceptSelf(int* nums, int numsSize, int* returnSize){
+    int* ret = (int*)malloc(sizeof(int)*numsSize);
+    int  product = nums[0];
 
+    for (int i = 1; i < numsSize; i++)
+    {
+        product *= nums[i];
+    }
+
+    for (int i = 0; i < numsSize; i++)
+    {
+        ret[i] = product / nums[i];
+    }
+    
+    *returnSize = numsSize;
+    return ret;
 }
