ad09d363343f1c1c7ca3e37c264ae406d56a8be7
邹明哲 <zoumingzhe@qq.com>
2020-06-08 13:40:02 +0800

    Update 238.product-of-array-except-self.c
    modify error

--- problems/238.product-of-array-except-self.c
+++ problems/238.product-of-array-except-self.c
@@ -7,15 +7,15 @@ int* productExceptSelf(int* nums, int numsSize, int* returnSize){
     int* tmp2 = (int*)malloc(sizeof(int)*(numsSize));
 
     tmp1[0] = 1;
-    for (int i = 1; i < numsSize; i++)
+    for (int i = 0; i < numsSize - 1; i++)
     {
-        tmp1[i] = tmp1[i-1]*nums[i];
+        tmp1[i+1] = tmp1[i]*nums[i];
     }
 
     tmp2[numsSize - 1] = 1;
-    for (int i = numsSize - 1; i >= 0; i--)
+    for (int i = numsSize - 1; i > 0; i--)
     {
-        tmp2[i-1] = tmp2[i]*nums[i-1];
+        tmp2[i-1] = tmp2[i]*nums[i];
     }
 
     for (int i = 0; i < numsSize; i++)
